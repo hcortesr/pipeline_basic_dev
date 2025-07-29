b354b1be4010c246d7fcd22311d96441c9d38398
Hector <hectorcortesr04@gmail.com>
2025-07-29 12:09:25 -0600

    Dockerized app

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:22-alpine3.22
-
-COPY package*.json ./
-
-RUN npm install
-
-COPY . .
-
-CMD ["npm", "test"]
--- server.js
+++
@@ -1,7 +0,0 @@
-import app from ".";
-
-// Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
