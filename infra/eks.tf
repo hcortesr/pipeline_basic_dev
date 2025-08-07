ae67ce7018c06c51ff4ee292ed79925b995b0613
Hector <hectorcortesr04@gmail.com>
2025-08-07 14:49:58 -0600

    The infra workflow was commented. The eks.tf file was added. An addon in AWS took to much time to start

---
+++ infra/eks.tf
@@ -0,0 +1,46 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "my-cluster"
+  kubernetes_version = "1.33"
+
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
+    }
+  }
+
+  # Optional
+  endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = "vpc-0d32dfc0080934120"
+  subnet_ids               = ["subnet-009e143a4c1a9fd75", "subnet-01efa1873f7ce2171"]
+  control_plane_subnet_ids = ["subnet-009e143a4c1a9fd75", "subnet-01efa1873f7ce2171"]
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t2.micro"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
